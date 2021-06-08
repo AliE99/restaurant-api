97f84fe6fc4c3cdc39c9a7c288451a73fd7a1a4f
Ali <alielahi.1378@gmail.com>
2021-06-08 11:51:17 +0430

    Feat : Restaurant API

---
+++ app/routes/restaurant.routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+    const restaurants = require('../controllers/restaurant.controller.js');
+
+    // Create a new restaurant
+    app.post('/restaurants', restaurants.create);
+
+    // Retrieve all restaurants
+    app.get('/restaurants', restaurants.findAll);
+
+    // Retrieve a single restaurant with restaurantId
+    app.get('/restaurants/:restaurantId', restaurants.findOne);
+
+    // Update a restaurant with restaurantId
+    app.put('/restaurants/:restaurantId', restaurants.update);
+
+    // Delete a restaurant with restaurantId
+    app.delete('/restaurants/:restaurantId', restaurants.delete);
+}
