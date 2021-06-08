ab9d237cda300e2ccfd89844302f597a329ed330
Ali <alielahi.1378@gmail.com>
2021-06-08 12:17:16 +0430

    Feat : order Api

--- app/routes/restaurant.routes.js
+++ app/routes/restaurant.routes.js
@@ -1,18 +1,21 @@
 module.exports = (app) => {
-    const restaurants = require('../controllers/restaurant.controller.js');
+    const restaurants = require("../controllers/restaurant.controller.js");
 
     // Create a new restaurant
-    app.post('/restaurants', restaurants.create);
+    app.post("/restaurants", restaurants.create);
 
     // Retrieve all restaurants
-    app.get('/restaurants', restaurants.findAll);
+    app.get("/restaurants", restaurants.findAll);
 
     // Retrieve a single restaurant with restaurantId
-    app.get('/restaurants/:restaurantId', restaurants.findOne);
+    app.get("/restaurants/:restaurantId", restaurants.findOne);
+
+    // Retrieve a single restaurants menu
+    app.get("/restaurants/menu/:restaurantId", restaurants.menu);
 
     // Update a restaurant with restaurantId
-    app.put('/restaurants/:restaurantId', restaurants.update);
+    app.put("/restaurants/:restaurantId", restaurants.update);
 
     // Delete a restaurant with restaurantId
-    app.delete('/restaurants/:restaurantId', restaurants.delete);
-}
+    app.delete("/restaurants/:restaurantId", restaurants.delete);
+};
